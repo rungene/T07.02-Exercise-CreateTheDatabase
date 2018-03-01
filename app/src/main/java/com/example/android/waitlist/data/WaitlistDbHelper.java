581f7f95c8f5bc4fa621eddec4b79efbc685b500
rungene <rungenesamuel@gmail.com>
2018-03-01 16:04:39 +0300

    working on DB Helper: WaitlistDbhelper

--- app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,11 +1,23 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper{
+
 
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
 
+    public static final String DATABASE_NAME = "waitlist.db";
+
     // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+    public static final int DATABASE_VERSION = 1;
+
+    public WaitlistDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     // TODO (4) Create a Constructor that takes a context and calls the parent constructor
 
@@ -19,4 +31,24 @@ public class WaitlistDbHelper {
 
         // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
 
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " +
+                WaitlistContract.WaitlistEntry.TABLE_NAME + " ("+
+                WaitlistContract.WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME + "TEXT NOT NULL, " +
+                WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL,"+
+                WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
+
+
+sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WaitlistContract.WaitlistEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
 }
